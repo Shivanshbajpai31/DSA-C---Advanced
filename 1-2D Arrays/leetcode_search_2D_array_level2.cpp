170cc2077d9f627524028c237facf139fa1288dd
Shivansh Bajpai <shivanshbajpai31@gmail.com>
2024-10-07 17:54:51 +0530

    committed

--- 1-2D Arrays/leetcode_search_2D_array_level2.cpp
+++ 1-2D Arrays/leetcode_search_2D_array_level2.cpp
@@ -3,25 +3,30 @@ using namespace std;
 
 
 bool searchMatrix(vector<vector< int >> & matrix, int target){
-    int row=matrix.size();
-    int col= matrix[0].size();
+  int row= matrix.size();
+  int col=matrix[0].size();
 
-    int rowIndex=0;
-    int colIndex=col-1;
+  int start=0;
+  int end=row*col-1;
 
-    while ( rowIndex < row && colIndex>=0){
-        int element= matrix[rowIndex][colIndex];
+  int mid=start+(end-start)/2;
 
-        if (element == target){
-            return 1;
-        }
-        if (element < target){
-            rowIndex++;
-        }
-        else{
-            colIndex--;
-        }
+  while (start <=end){
 
-}
-return 0;
+  int element=matrix[mid/col] [mid%col];
+
+  if (element == target){
+    return 1;
+  }
+  if (element <=target){
+    start =mid+1;
+
+  }
+  else{
+    end=mid-1;
+  }
+  mid =start+(end-start)/2;
+  }
+  return 0;
+
 }
