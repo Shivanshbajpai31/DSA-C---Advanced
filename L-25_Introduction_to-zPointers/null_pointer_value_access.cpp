4f3ba3fd8275b7f21e3d87d95afdedd4c4bf5605
Shivansh Bajpai <shivanshbajpai31@gmail.com>
2024-10-15 00:53:43 +0530

    committed

---
+++ L-25_Introduction_to-zPointers/null_pointer_value_access.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int i=5;
+    int *q=&i;
+    cout << q<< endl;
+    cout << *q << endl;
+
+    int *p=0;
+    p=&i;
+    cout << p <<endl;
+    cout << *p <<endl;
+
+    int num=5;
+    int a=num;
+    cout << "a before " << num << endl;
+    a++;
+     cout << "a after " << num << endl;
+
+    int *p=&num;
+    cout << "before " << num << endl;
+    (*p)++;
+    cout << "after " << num << endl;
+
+    return 0;
+    
+
+
+}
