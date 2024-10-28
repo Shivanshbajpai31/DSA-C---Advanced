adf1285d0c1050c5434cc44f13fb83b37ef0e57b
Shivansh Bajpai <shivanshbajpai31@gmail.com>
2024-10-28 22:30:44 +0530

    committed

---
+++ L-26_Pointers2/3-Character_arrays.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int arr[5]={1,2,4,5,6};
+    char  ch[6]="abcde";
+
+    cout << arr << endl;
+    cout << ch << endl;
+
+    char *c=&ch[0];
+    return 0;
+}
