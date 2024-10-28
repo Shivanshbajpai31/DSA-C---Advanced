df7c7405ede7ec5435864a56bf1548f51756ba14
Shivansh Bajpai <shivanshbajpai31@gmail.com>
2024-10-28 22:14:12 +0530

    committed

--- L-26_Pointers2/2-Arrays/Pointers_differmce.cpp
+++ L-26_Pointers2/2-Arrays/Pointers_differmce.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(){
     int temp[10]={1,2,3,4,5};
-
+/*
     cout << sizeof(temp) << endl; // for array it will give 40
     cout << sizeof(*temp) << endl; //  it will give 4 bytes
     cout << sizeof(&temp) << endl; // 8 bytes
@@ -12,7 +12,13 @@ int main(){
     int *ptr=&temp[0];
     cout << sizeof(ptr) << endl; // it will 8 bytes
     cout << sizeof(*ptr) << endl; // gives 4
-    cout << sizeof(&ptr) << endl; // gives 4
+    cout << sizeof(&ptr) << endl; // gives 4 */
+
+    // for addresss
+    cout << "1 is" << &temp[0] << endl;
+
+    int *p=&temp[0];
+    cout << "2" << &p << endl;
 
 
     return 0;
