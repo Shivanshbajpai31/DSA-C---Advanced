aeb599a4ae8237edefd0a35650bd912a0dfec83b
Shivansh Bajpai <shivanshbajpai31@gmail.com>
2024-10-29 20:37:08 +0530

    committed

--- L-26_Pointers2/1-pointers_main.cpp
+++ L-26_Pointers2/1-pointers_main.cpp
@@ -5,14 +5,14 @@ int main(){
     int arr[10]={11,22,33,44,55};
 
     cout << "Address of 1st is :" << arr << endl;
-    cout << arr[0] << endl;
+    cout << arr[0] << endl;  //gives value of o index
 
     cout << "Address is " << &arr[0] << endl; // gives addressss
 
-    cout << "4th is " << *arr << endl;
-    cout << "5th is " << *arr+1 << endl; // it will update the valuesss
-    cout << " 6th is :" << *(arr+1) << endl;
-    cout << "7th is " << *(arr)+1 << endl; // update the value 
+    cout << "4th is " << *arr << endl;//first memory will print i.e is 11
+    cout << "5th is " << *arr+1 << endl; // it will increment the first array values the valuesss
+    cout << " 6th is :" << *(arr+1) << endl; // it will give 22
+    cout << "7th is " << *(arr)+1 << endl; // Same things happen in 5th printing statement
     cout << "8th is " << arr[2] << endl;
     cout << "9th is " << *(arr+2) << endl; // gives 33
 
