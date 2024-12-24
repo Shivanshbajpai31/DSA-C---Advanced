38ea722cacce3476ab2d0e4a5cd902640afe4829
Shivansh Bajpai <shivanshbajpai31@gmail.com>
2024-12-24 21:15:39 +0530

    committed

---
+++ 3-Arrays/a_array_traversal.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int arr[]={1,2,3,4,5};
+    int len= sizeof(arr) / sizeof(arr[0]);
+    for(int i=0 ; i<len ; i++){
+        cout << arr[i] << endl;
+    }
+}
